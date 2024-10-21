fe3e8ba856a8adbbec4d6a267424d9c87277aeb0
rawhialfar <alfarrawhi@gmail.com>
2024-10-20 21:12:39 -0400

    fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,44 +11,62 @@ const Navbar = () => {
 
 	return (
 		<nav className="flex h-20 justify-between items-center py-5 px-10 fixed top-0 left-0 right-0 z-20">
-			<div className="text-3xl font-bold">
+			<div className="text-3xl font-bold space-x-8 flex">
 				<a
 					href="#hero"
-					className="hover:text-gray-400 cursor-pointer relative z-50"
+					className="hover:text-gray-400 relative transition-transform duration-300 ease-in-out hover:-translate-y-1"
 				>
 					Rawhi Alfar
 				</a>
 			</div>
 
 			{/* Menu for larger screens */}
-			<div className="relative z-50 hidden md:flex space-x-8">
-				<a href="#about" className="hover:text-gray-400">
+			<div className="relative hidden md:flex space-x-8 ">
+				<a
+					href="#about"
+					className="hover:text-gray-400 transition-transform duration-300 ease-in-out hover:-translate-y-1"
+				>
 					About Me
 				</a>
-				<a href="#experience" className="hover:text-gray-400">
+				<a
+					href="#experience"
+					className="hover:text-gray-400 transition-transform duration-300 ease-in-out hover:-translate-y-2"
+				>
 					Experience
 				</a>
-				<a href="#projects" className="hover:text-gray-400">
+				<a
+					href="#projects"
+					className="hover:text-gray-400 transition-transform duration-300 ease-in-out hover:-translate-y-2"
+				>
 					Projects
 				</a>
 			</div>
 
 			{/* Icons and Resume link for larger screens */}
 			<div className="relative z-50 hidden md:flex items-center space-x-4">
-				<a href="https://www.linkedin.com/in/rawhi-alfar" className="text-xl">
+				<a
+					href="https://www.linkedin.com/in/rawhi-alfar"
+					className="text-xl transition-transform duration-300 ease-in-out hover:-translate-y-1"
+				>
 					<i className="bx bxl-linkedin"></i>
 				</a>
-				<a href="https://github.com/rawhialfar" className="text-xl">
+				<a
+					href="https://github.com/rawhialfar"
+					className="text-xl transition-transform duration-300 ease-in-out hover:-translate-y-1"
+				>
 					<i className="bx bxl-github"></i>
 				</a>
-				<a href="mailto:alfarrawhi@gmail.com" className="text-xl">
+				<a
+					href="mailto:alfarrawhi@gmail.com"
+					className="text-xl transition-transform duration-300 ease-in-out hover:-translate-y-1"
+				>
 					<i className="bx bx-envelope"></i>
 				</a>
 				<a
 					href={Resume}
 					target="_blank"
 					rel="noopener noreferrer"
-					className="bg-gray-700 py-2 px-4 rounded-lg hover:bg-gray-600"
+					className="bg-gray-700 py-2 px-4 rounded-lg hover:bg-gray-600 hover:text-lg transition-all duration-300 ease-in-out"
 				>
 					Resume
 				</a>
